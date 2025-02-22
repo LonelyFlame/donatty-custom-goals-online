177656fcd8a9a31ba3d44d0b3c34a9fc1aef8d67
Max Chursin <m.chursin@glorax.com>
2025-02-22 22:33:06 +0300

    feat: Implemented AntD

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    prependData: `
+      @import "@/theme/variables.scss";
+      @import "@/theme/mixins.scss";
+      @import "@/theme/typography.scss";
+    `,
+  },
 };
 
 export default nextConfig;
